bb9e87c60e3a2355e2e9afc45920720e2454bb7c
kyle <kyle@101010.org>
2025-05-14 14:51:29 -0500

    add scraping script

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # solar_trmnl
+
+Repo to contain tools to:
+* Scrape basic info off of a solar website (using browserless/chrome)
+* Generate a BMP image of that info
+* Upload that BMP to a public S3 bucket where it can be downloaded via TMRNL
+
---
+++ scrape_glcsolar.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+PATH=/bin:/usr/bin
+
+BROWSERLESS_UP=$(docker ps | grep browserless | grep -c 'Up')
+
+if [[ "$BROWSERLESS_UP" -lt 1 ]]; then
+  docker start -p 3000:3000 browserless/chrome
+  sleep 15
+fi
+if [[ "$BROWSERLESS_UP" -lt 1 ]]; then
+  echo "Error: cannot find browserless/chrome"
+  exit 2
+fi
+
+TMP_HTML=$(mktemp)
+
+curl -o - \
+  -X POST http://localhost:3000/scrape \
+  -H 'Content-Type: application/json' \
+  -d '{
+         "url": "https://monitoringpublic.solaredge.com/solaredge-web/p/site/public?name=Gethsemane%20Lutheran%20Church",
+         "gotoOptions": { "waitUntil": "networkidle0" },
+  "elements": [
+    {
+      "selector": "body"
+    }
+  ]
+}' | jq .data[].results[].html > $TMP_HTML
+
+echo "wrote to $TMP_HTML"
+
+# 294.34 kWh
+ENERGY_TODAY=$(sed 's#^.*Energy today</div>##' "$TMP_HTML" | sed 's#<div[^>]*>##' | cut -c 1-100 | sed 's/<.*$//')
+LIFETIME_ENERGY=$(sed 's#^.*Lifetime energy</div>##' "$TMP_HTML" | sed 's#<div[^>]*>##' | cut -c 1-100 | sed 's/<.*$//')
+
+rm "$TMP_HTML"
